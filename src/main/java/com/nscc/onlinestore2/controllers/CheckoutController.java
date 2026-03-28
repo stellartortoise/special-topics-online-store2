ca8f9ea7bb185170ce5472101277abe44d59fd3d
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2026-03-27 21:27:00 -0300

    Add Stripe checkout controller and dependency

---
+++ src/main/java/com/nscc/onlinestore2/controllers/CheckoutController.java
@@ -0,0 +1,85 @@
+package com.nscc.onlinestore2.controllers;
+
+
+import com.nscc.onlinestore2.service.GameService;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin    // enables cors
+@RestController
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private final GameService gameService;
+
+    @Value("${stripe.secret.key}")
+    private String stripeSecretKey;
+
+
+    @PostConstruct
+    public void init() { Stripe.apiKey = stripeSecretKey; }
+
+    public CheckoutController(GameService gameService) {
+        this.gameService = gameService;
+
+
+        //Stripe.apiKey = stripeSecretKey;
+    }
+
+    @PostMapping("/create-checkout-session")
+    public Map<String, String> createCheckoutSession(/* Cart */) throws StripeException
+    {
+        String YOUR_DOMAIN = "http://localhost:5173";
+
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .setUiMode(SessionCreateParams.UiMode.EMBEDDED)
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setReturnUrl(YOUR_DOMAIN + "/confirmation?session_id={CHECKOUT_SESSION_ID}")
+                        .addLineItem(
+                                SessionCreateParams.LineItem.builder()
+                                        .setQuantity(1L)
+                                        // Provide the exact Price ID of the product you want to sell
+                                        // .setPrice("{{PRICE_ID}}")
+                                        .setPriceData(
+                                                SessionCreateParams.LineItem.PriceData.builder()
+                                                        .setCurrency("cad")
+                                                        .setUnitAmount(2500L)
+                                                        .setProductData(
+                                                                SessionCreateParams.LineItem.PriceData.ProductData
+                                                                        .builder()
+                                                                        .setName("Video Game")
+                                                                        .build()
+                                                        )
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+
+        Session session = Session.create(params);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("clientSecret", session.getClientSecret());
+        return map;
+    }
+
+    @GetMapping("session-status")
+    public Map<String, String> getSessionStatus(@RequestParam("session_id") String sessionId) throws StripeException {
+        Session session = Session.retrieve(sessionId);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("status", session.getStatus());
+        map.put("customer_email", session.getCustomerDetails().getEmail());
+
+        return map;
+    }
+}
